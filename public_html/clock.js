b770809edfc2521f25dd9e8ac1160206c840b287
Fenila <100304937+fenilaphilip@users.noreply.github.com>
2022-02-26 15:39:00 +0100

    Using bootstrap 5

--- public_html/clock.js
+++ public_html/clock.js
@@ -6,13 +6,11 @@ function update() {
 // shows current date
 
     document.getElementById("date").innerHTML = current_date;
-
 //let time= new time();
     let time = now.getHours() + ":" + now.getMinutes()
             + ":" + now.getSeconds();
 // shows current time
     document.getElementById("time").innerHTML = time;
-
 // shows which day is
     let day = now.getDay();
     switch (day) {
@@ -39,23 +37,49 @@ function update() {
             break;
     }
     document.getElementById("day").innerHTML = day;
-
-
-
 // 12 hours format
     let hours = now.getHours();
     if (hours > 12) {
         hours = hours % 12;
         time = hours + ":" + now.getMinutes()
                 + ":" + now.getSeconds() + " PM";
-
         document.getElementById("time_twelve").innerHTML = time;
     } else {
         time = hours + ":" + now.getMinutes()
                 + ":" + now.getSeconds() + " AM";
         document.getElementById("time_twelve").innerHTML = time;
     }
- 
+ console.log(hours)
+// for the Greetings
+    let greet = "";
+    let wish = "";
+    if (6 <= hours && hours < 10) {
+        greet = "Good Morning";
+        wish = " Have a Good Day!";
+    } else if (10 <= hours && hours < 1) {
+        greet = "Hello";
+        wish = " Have a Good Day!";
+
+    } else if (1 <= hours && hours < 4) {
+        greet = "Good Afternoon";
+        wish = " Have a Good Day!";
+
+    } else if (4 <= hours && hours < 10) {
+        greet = "Good Evening";
+        wish = " Have a nice evening!";
+
+    } else if (10 <= hours && hours < 6) {
+        greet = "Good Night";
+        wish = " Have a good sleep!";
+
+    }
+    document.getElementById("greeting").innerHTML = greet + "<br>" + wish;
+
+
+
+
+
 }
 
-update();
+
+    update();
